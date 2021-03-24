fba68fb58153d0cd81bd10a3ee50f20f95a265ab
WeronikaNuszkiewicz <weronika.nuszkiewicz@wp.pl>
2021-03-24 19:51:49 +0100

    add_serializers

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from skniprojectmain import views
 
+from django.contrib import admin
+from django.urls import path, include
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('measures/', views.company_measures, name='company_measures'),
+    #path('measures/', views.company_measures, name='company_measures'),
+    path('', include('skniprojectmain.urls')),
 ]
